e542531c17e3bac26c89f77debc3d4faeac897f3
Derek Harnett <DHarnett.dev@proton.me>
2026-03-31 21:42:20 -0700

    add worker, job_category, site, building, and zone tables

--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -5,11 +5,59 @@
 -- Version : 0.1.0
 -- --------------------------------------------------------------------------
 
-DROP TABLE IF EXISTS test;
+DROP TABLE IF EXISTS job_category;
+CREATE TABLE job_category (
+    job_code INT AUTO_INCREMENT,
+    job_title VARCHAR(80) NOT NULL UNIQUE,
+    job_desc TEXT,
+    PRIMARY KEY (job_code)
+);
 
-CREATE TABLE test (
-    col_1 INT,
-    col_2 VARCHAR(30),
-    col_3 DATETIME,
-    col_4 DECIMAL(7, 2)
+DROP TABLE IF EXISTS worker;
+CREATE TABLE worker (
+    worker_id INT AUTO_INCREMENT,
+    worker_fname VARCHAR(40) NOT NULL,
+    worker_initials VARCHAR(5),
+    worker_lname VARCHAR(40) NOT NULL,
+    worker_sin CHAR(9) NOT NULL UNIQUE,
+    worker_phone CHAR(10) NOT NULL,
+    worker_email VARCHAR(80) NOT NULL,
+    job_code INT NOT NULL,
+    PRIMARY KEY (worker_id),
+    FOREIGN KEY (job_code) REFERENCES job_category (job_code) ON UPDATE CASCADE
+);
+
+DROP TABLE IF EXISTS site;
+CREATE TABLE site (
+    site_num INT AUTO_INCREMENT,
+    site_name VARCHAR(80) NOT NULL,
+    site_desc TEXT,
+    site_lat DECIMAL(5, 3) NOT NULL CHECK (site_lat BETWEEN -90.0000 AND 90.0000),
+    site_long DECIMAL (6, 3) NOT NULL CHECK (site_long BETWEEN -180.0000 AND 180.0000),
+    site_region VARCHAR(80) NOT NULL,
+    site_country VARCHAR(80) NOT NULL,
+    PRIMARY KEY (site_num),
+    CONSTRAINT unique_location UNIQUE (site_lat, site_long)
+);
+
+DROP TABLE IF EXISTS building;
+CREATE TABLE building (
+    bldg_num INT NOT NULL,
+    bldg_name VARCHAR(80) NOT NULL,
+    bldg_desc TEXT,
+    site_num INT NOT NULL,
+    PRIMARY KEY (site_num, bldg_num),
+    FOREIGN KEY (site_num) REFERENCES site (site_num) ON UPDATE CASCADE
+); 
+
+DROP TABLE IF EXISTS zone;
+CREATE TABLE zone (
+    zone_id INT AUTO_INCREMENT,
+    zone_num INT NOT NULL,
+    zone_desc TEXT,
+    bldg_num INT NOT NULL,
+    site_num INT NOT NULL,
+    PRIMARY KEY (zone_id),
+    CONSTRAINT unique_site_bldg_zone UNIQUE (site_num, bldg_num, zone_num),
+    FOREIGN KEY (site_num, bldg_num) REFERENCES building (site_num, bldg_num) ON UPDATE CASCADE
 );
