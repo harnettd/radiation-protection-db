6d567391e13e448aa9c30d05ca694c6407663958
Derek Harnett <DHarnett.dev@proton.me>
2026-04-06 20:28:25 -0700

    update view definitions

--- sql/create-views.sql
+++ sql/create-views.sql
@@ -5,8 +5,8 @@
 -- Version : 0.1.0
 -- --------------------------------------------------------------------------
 
--- Generate a calibration report showing each piece of equipment that is
--- overdue for calibration.
+-- Generate a report showing each piece of equipment that is overdue 
+-- for calibration.
 DROP VIEW IF EXISTS calibration_report;
 CREATE VIEW calibration_report AS
     SELECT
@@ -20,23 +20,8 @@ CREATE VIEW calibration_report AS
     WHERE DATEDIFF(eq_next_cal_date, CURRENT_DATE) < 0
     ORDER BY days_overdue, eq_cat_name;
 
--- Generate a report showing summary area statistics (avg and max) for each
--- zone. Exclude samples marked void.
-DROP VIEW IF EXISTS area_summary;
-CREATE VIEW area_summary AS
-    SELECT
-        site_num, bldg_num, zone_num,
-        ROUND(AVG(arr_concentration_bq_m3), 1) AS avg_radon_concentration,
-        ROUND(MAX(arr_concentration_bq_m3), 1) AS max_radon_concentration
-    FROM sample JOIN
-        area_radon_result USING (sample_id) JOIN
-        zone USING (zone_id)
-    WHERE sample_is_void = FALSE
-    GROUP BY zone_id
-    ORDER BY zone_id;
-
--- Generate a list of all pieces of equipment for which a corresponding
--- sample has been marked void.
+-- Generate a report listing all pieces of equipment for which a 
+-- corresponding sample has been marked void.
 DROP VIEW IF EXISTS void_equipment;
 CREATE VIEW void_equipment AS
     SELECT
@@ -45,22 +30,48 @@ CREATE VIEW void_equipment AS
         eq_model
     FROM equipment
     WHERE eq_ser_num IN (
-        SELECT DISTINCT
-            eq_ser_num
+        SELECT DISTINCT eq_ser_num
         FROM sample JOIN
             equipment_sample USING (sample_id)
         WHERE sample_is_void = TRUE
         );
+
+-- Generate a report showing summary area statistics (avg and max) for each
+-- zone for each category of area sample. Exclude samples marked void.
+DROP VIEW IF EXISTS area_summary;
+CREATE VIEW area_summary AS
+    SELECT
+        COALESCE(arr.zone_id, aar.zone_id) AS zone_id,
+        site_num, bldg_num, zone_num,
+        ROUND(AVG(arr_concentration_bq_m3), 1) AS avg_radon_concentration,
+        ROUND(MAX(arr_concentration_bq_m3), 1) AS max_radon_concentration,
+        ROUND(AVG(aar_concentration_bq_m3), 1) AS avg_alpha_concentration,
+        ROUND(MAX(aar_concentration_bq_m3), 1) AS max_alpha_concentration
+    FROM sample LEFT JOIN
+        area_radon_result arr USING (sample_id) LEFT JOIN
+        area_alpha_result aar USING (sample_id) JOIN
+        zone ON COALESCE(arr.zone_id, aar.zone_id) = zone.zone_id
+    WHERE sample_is_void = FALSE AND
+        samp_cat_code IN (1, 2, 3)
+    GROUP BY 
+        COALESCE(arr.zone_id, aar.zone_id)
+    ORDER BY zone_id;
+
 
 -- Generate a report showing how many days have passed since each type of
 -- area-sample has been taken for each zone.
 DROP VIEW IF EXISTS area_frequency;
 CREATE VIEW area_frequency AS
     SELECT
+        COALESCE(arr.zone_id, aar.zone_id) as zone_id,
         site_num, bldg_num, zone_num,
-        DATEDIFF(CURRENT_DATE, DATE(MAX(sample_start))) AS days_since_radon_sample
-    FROM sample JOIN
-        area_radon_result USING (sample_id) JOIN
-        zone USING (zone_id)
-    GROUP BY zone_id
-    ORDER BY zone_id;
+        samp_cat_name,
+        MIN(DATEDIFF(CURRENT_DATE, DATE(sample_start))) AS days_since
+    FROM sample LEFT JOIN
+        area_radon_result arr USING (sample_id) LEFT JOIN
+        area_alpha_result aar USING (sample_id) JOIN
+        sample_category USING (samp_cat_code) JOIN
+        zone ON COALESCE(arr.zone_id, aar.zone_id) = zone.zone_id
+    WHERE sample_is_void = FALSE
+    GROUP BY COALESCE(arr.zone_id, aar.zone_id), samp_cat_code
+    ORDER BY zone_id, samp_cat_code;
